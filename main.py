8e5263a18ab907d4d9fc09fcac2d38a85388e175
Noman14611 <noumantareen316@gmail.com>
2025-06-15 12:02:19 +0500

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import streamlit as st
 from feeapp import run_fee_app
-
 from attendanceapp import run_attendance_app
+from salaryapp import run_salary_app
 
-st.set_page_config(page_title="🏫 School Management", layout="wide")
-st.sidebar.title("📂 Select Application")
+st.set_page_config(page_title="School App", layout="wide")
+st.sidebar.title("🏫 School Management")
 
-selected = st.sidebar.selectbox("📌 Choose App", ["📅 Attendance", "💰 Fee Manager"])
+app = st.sidebar.selectbox("Select App", [
+    "📋 Attendance Tracker",
+    "💰 Fee Records",
+    "👨‍🏫 Teacher Salary"
+])
 
-if selected == "📅 Attendance":
+if app == "📋 Attendance Tracker":
     run_attendance_app()
-elif selected == "💰 Fee Manager":
+elif app == "💰 Fee Records":
     run_fee_app()
+elif app == "👨‍🏫 Teacher Salary":
+    run_salary_app()
